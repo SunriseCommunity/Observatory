e35032107829a0094a71441b344200d8d4a17e01
ItsRichardScull <totallynotrichardscull@gmail.com>
2024-11-04 15:40:27 +0200

    chore: create describe for each mirror in tests; chore: Ignore bancho if no token is provided; chore: Edit .env.example

--- server/src/tests/mirror.test.ts
+++ server/src/tests/mirror.test.ts
@@ -1,9 +1,10 @@
-import { describe, expect, it } from 'bun:test';
+import { beforeEach, describe, expect, it } from 'bun:test';
 import { BanchoClient, DirectClient } from '../core/domains';
 
 // @ts-ignore
 import json from './data/mirror.tests.json';
 import { MinoClient } from '../core/domains/catboy.best/mino.client';
+import config from '../config';
 
 describe('Mirror tests', () => {
     const banchoClient = new BanchoClient();
@@ -16,123 +17,150 @@ describe('Mirror tests', () => {
         ];
     };
 
-    it('Bancho: should return converted beatmap', async () => {
-        const randomTest = getRandomTest('getBeatmapById');
-        const { beatmapId } = randomTest;
+    describe('Bancho tests', () => {
+        const hasClientToken =
+            config.BANCHO_CLIENT_ID && config.BANCHO_CLIENT_SECRET
+                ? true
+                : false;
 
-        const beatmap = await banchoClient.getBeatmap({
-            beatmapId,
+        it('Bancho: should return converted beatmap', async () => {
+            if (!hasClientToken) {
+                return it.skip('Bancho: should return converted beatmap', () => {});
+            }
+
+            const randomTest = getRandomTest('getBeatmapById');
+            const { beatmapId } = randomTest;
+
+            const beatmap = await banchoClient.getBeatmap({
+                beatmapId,
+            });
+
+            expect(beatmap.result).toContainAllKeys(
+                Object.keys(randomTest.data),
+            );
         });
 
-        expect(beatmap.result).toContainAllKeys(Object.keys(randomTest.data));
+        it('Bancho: should return converted beatmapset', async () => {
+            if (!hasClientToken) {
+                return it.skip('Bancho: should return converted beatmapset', () => {});
+            }
+
+            const randomTest = getRandomTest('getBeatmapsetById');
+            const { beatmapSetId } = randomTest;
+
+            const beatmap = await banchoClient.getBeatmapSet({
+                beatmapSetId,
+            });
+
+            expect(beatmap.result).toContainAllKeys(
+                Object.keys(randomTest.data),
+            );
+        });
     });
 
-    it('Bancho: should return converted beatmapset', async () => {
-        const randomTest = getRandomTest('getBeatmapsetById');
-        const { beatmapSetId } = randomTest;
+    describe('Mino tests', () => {
+        it('Mino: should return converted beatmap', async () => {
+            const randomTest = getRandomTest('getBeatmapById');
+            const { beatmapId } = randomTest;
 
-        const beatmap = await banchoClient.getBeatmapSet({
-            beatmapSetId,
+            const beatmap = await minoClient.getBeatmap({
+                beatmapId,
+            });
+
+            expect(beatmap.result).toContainAllKeys(
+                Object.keys(randomTest.data),
+            );
         });
 
-        expect(beatmap.result).toContainAllKeys(Object.keys(randomTest.data));
+        it('Mino: should return converted beatmapset', async () => {
+            const randomTest = getRandomTest('getBeatmapsetById');
+            const { beatmapSetId } = randomTest;
+
+            const beatmap = await minoClient.getBeatmapSet({
+                beatmapSetId,
+            });
+
+            expect(beatmap.result).toContainAllKeys(
+                Object.keys(randomTest.data),
+            );
+        });
+
+        it('Mino: should download beatmap set without video', async () => {
+            const randomTest = getRandomTest('downloadBeatmapSet');
+            const { beatmapSetId } = randomTest;
+
+            const beatmap = await minoClient.downloadBeatmapSet({
+                beatmapSetId,
+                noVideo: true,
+            });
+
+            const expected = await Bun.file(
+                `${import.meta.dir}/data/${beatmapSetId}n.osz`,
+            ).arrayBuffer();
+
+            expect(beatmap.result?.byteLength).toBeWithin(
+                expected.byteLength - 1000,
+                expected.byteLength + 1000,
+            );
+        }, 30000);
+
+        it('Mino: should download beatmap set with video', async () => {
+            const randomTest = getRandomTest('downloadBeatmapSet');
+            const { beatmapSetId } = randomTest;
+
+            const beatmap = await minoClient.downloadBeatmapSet({
+                beatmapSetId,
+                noVideo: false,
+            });
+
+            const expected = await Bun.file(
+                `${import.meta.dir}/data/${beatmapSetId}.osz`,
+            ).arrayBuffer();
+
+            expect(beatmap.result?.byteLength).toBeWithin(
+                expected.byteLength - 1000,
+                expected.byteLength + 1000,
+            );
+        }, 30000);
     });
 
-    it('Mino: should return converted beatmap', async () => {
-        const randomTest = getRandomTest('getBeatmapById');
-        const { beatmapId } = randomTest;
+    describe('Direct tests', () => {
+        it('Direct: should download beatmap set without video', async () => {
+            const randomTest = getRandomTest('downloadBeatmapSet');
+            const { beatmapSetId } = randomTest;
 
-        const beatmap = await minoClient.getBeatmap({
-            beatmapId,
-        });
+            const beatmap = await directClient.downloadBeatmapSet({
+                beatmapSetId,
+                noVideo: true,
+            });
 
-        expect(beatmap.result).toContainAllKeys(Object.keys(randomTest.data));
+            const expected = await Bun.file(
+                `${import.meta.dir}/data/${beatmapSetId}n.osz`,
+            ).arrayBuffer();
+
+            expect(beatmap.result?.byteLength).toBeWithin(
+                expected.byteLength - 1000,
+                expected.byteLength + 1000,
+            );
+        }, 30000);
+
+        it('Direct: should download beatmap set with video', async () => {
+            const randomTest = getRandomTest('downloadBeatmapSet');
+            const { beatmapSetId } = randomTest;
+
+            const beatmap = await directClient.downloadBeatmapSet({
+                beatmapSetId,
+                noVideo: false,
+            });
+
+            const expected = await Bun.file(
+                `${import.meta.dir}/data/${beatmapSetId}.osz`,
+            ).arrayBuffer();
+
+            expect(beatmap.result?.byteLength).toBeWithin(
+                expected.byteLength - 1000,
+                expected.byteLength + 1000,
+            );
+        }, 30000);
     });
-
-    it('Mino: should return converted beatmapset', async () => {
-        const randomTest = getRandomTest('getBeatmapsetById');
-        const { beatmapSetId } = randomTest;
-
-        const beatmap = await minoClient.getBeatmapSet({
-            beatmapSetId,
-        });
-
-        expect(beatmap.result).toContainAllKeys(Object.keys(randomTest.data));
-    });
-
-    it('Mino: should download beatmap set without video', async () => {
-        const randomTest = getRandomTest('downloadBeatmapSet');
-        const { beatmapSetId } = randomTest;
-
-        const beatmap = await minoClient.downloadBeatmapSet({
-            beatmapSetId,
-            noVideo: true,
-        });
-
-        const expected = await Bun.file(
-            `${import.meta.dir}/data/${beatmapSetId}n.osz`,
-        ).arrayBuffer();
-
-        expect(beatmap.result?.byteLength).toBeWithin(
-            expected.byteLength - 1000,
-            expected.byteLength + 1000,
-        );
-    }, 30000);
-
-    it('Mino: should download beatmap set with video', async () => {
-        const randomTest = getRandomTest('downloadBeatmapSet');
-        const { beatmapSetId } = randomTest;
-
-        const beatmap = await minoClient.downloadBeatmapSet({
-            beatmapSetId,
-            noVideo: false,
-        });
-
-        const expected = await Bun.file(
-            `${import.meta.dir}/data/${beatmapSetId}.osz`,
-        ).arrayBuffer();
-
-        expect(beatmap.result?.byteLength).toBeWithin(
-            expected.byteLength - 1000,
-            expected.byteLength + 1000,
-        );
-    }, 30000);
-
-    it('Direct: should download beatmap set without video', async () => {
-        const randomTest = getRandomTest('downloadBeatmapSet');
-        const { beatmapSetId } = randomTest;
-
-        const beatmap = await directClient.downloadBeatmapSet({
-            beatmapSetId,
-            noVideo: true,
-        });
-
-        const expected = await Bun.file(
-            `${import.meta.dir}/data/${beatmapSetId}n.osz`,
-        ).arrayBuffer();
-
-        expect(beatmap.result?.byteLength).toBeWithin(
-            expected.byteLength - 1000,
-            expected.byteLength + 1000,
-        );
-    }, 30000);
-
-    it('Direct: should download beatmap set with video', async () => {
-        const randomTest = getRandomTest('downloadBeatmapSet');
-        const { beatmapSetId } = randomTest;
-
-        const beatmap = await directClient.downloadBeatmapSet({
-            beatmapSetId,
-            noVideo: false,
-        });
-
-        const expected = await Bun.file(
-            `${import.meta.dir}/data/${beatmapSetId}.osz`,
-        ).arrayBuffer();
-
-        expect(beatmap.result?.byteLength).toBeWithin(
-            expected.byteLength - 1000,
-            expected.byteLength + 1000,
-        );
-    }, 30000);
 });
