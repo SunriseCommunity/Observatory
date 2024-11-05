15fe5f831d74915ebec5694bf07a4a215b4425d4
ItsRichardScull <totallynotrichardscull@gmail.com>
2024-11-05 13:05:50 +0200

    feat: Add ?full query for v2/b and v2/md5

--- server/src/controllers/api/index.ts
+++ server/src/controllers/api/index.ts
@@ -6,11 +6,60 @@ export default (app: App) => {
     app.use(BeatmapsManagerPlugin)
         .get(
             'v2/b/:id',
-            ({ BeatmapsManagerInstance, params: { id } }) =>
-                BeatmapsManagerInstance.getBeatmap({ beatmapId: Number(id) }),
+            async ({
+                BeatmapsManagerInstance,
+                params: { id },
+                query: { full },
+            }) => {
+                const beatmap = await BeatmapsManagerInstance.getBeatmap({
+                    beatmapId: Number(id),
+                });
+
+                if (!beatmap.data) return beatmap;
+
+                console.log(full);
+
+                return full
+                    ? BeatmapsManagerInstance.getBeatmapSet({
+                          beatmapSetId: beatmap.data?.beatmapset_id,
+                      })
+                    : beatmap;
+            },
             {
                 params: t.Object({
                     id: t.Numeric(),
+                }),
+                query: t.Object({
+                    full: t.Optional(t.Boolean()),
+                }),
+                tags: ['v2'],
+            },
+        )
+        .get(
+            'v2/md5/:hash',
+            async ({
+                BeatmapsManagerInstance,
+                params: { hash },
+                query: { full },
+            }) => {
+                const beatmap = await BeatmapsManagerInstance.getBeatmap({
+                    beatmapHash: hash,
+                });
+
+                if (!beatmap.data) return beatmap;
+
+                return full
+                    ? BeatmapsManagerInstance.getBeatmapSet({
+                          beatmapSetId: beatmap.data?.beatmapset_id,
+                      })
+                    : beatmap;
+            },
+            {
+                params: t.Object({
+                    hash: t.String(),
+                }),
+                query: t.Object({
+                    full: t.Optional(t.Boolean()),
                 }),
                 tags: ['v2'],
             },
@@ -24,17 +73,6 @@ export default (app: App) => {
             {
                 params: t.Object({
                     id: t.Numeric(),
-                }),
-                tags: ['v2'],
-            },
-        )
-        .get(
-            'v2/md5/:hash',
-            ({ BeatmapsManagerInstance, params: { hash } }) =>
-                BeatmapsManagerInstance.getBeatmap({ beatmapHash: hash }),
-            {
-                params: t.Object({
-                    hash: t.String(),
                 }),
                 tags: ['v2'],
             },
