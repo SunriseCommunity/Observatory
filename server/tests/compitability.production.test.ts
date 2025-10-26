132979c7d4652edf96e361b0a4ce00490e803fce
richardscull <106016833+richardscull@users.noreply.github.com>
2025-10-26 06:13:06 +0200

    feat: Explain compitability tests purpose

--- server/tests/compitability.production.test.ts
+++ server/tests/compitability.production.test.ts
@@ -10,35 +10,35 @@ import { GatariClient } from '../src/core/domains/gatari.pw/gatari.client';
 import { OsulabsClient } from '../src/core/domains/beatmaps.download/osulabs.client';
 import { StorageManager } from '../src/core/managers/storage/storage.manager';
 
-describe('Mirror tests', () => {
-    const storageManager = new StorageManager();
+describe.skipIf(!config.IsProduction)(
+    'Production compatibility tests - USE TO CHECK IF THE MIRRORS ARE COMPATIBLE WITH THE PRODUCTION ENVIRONMENT',
+    () => {
+        const storageManager = new StorageManager();
 
-    const banchoClient = new BanchoClient();
-    const minoClient = new MinoClient(storageManager);
-    const osulabsClient = new OsulabsClient(storageManager);
-    const directClient = new DirectClient();
-    const nerinyanClient = new NerinyanClient();
-    const gatariClient = new GatariClient();
+        const banchoClient = new BanchoClient();
+        const minoClient = new MinoClient(storageManager);
+        const osulabsClient = new OsulabsClient(storageManager);
+        const directClient = new DirectClient();
+        const nerinyanClient = new NerinyanClient();
+        const gatariClient = new GatariClient();
 
-    const getRandomTest = (key: string) => {
-        return json.tests[key][
-            Math.floor(Math.random() * json.tests[key].length)
-        ];
-    };
+        const getRandomTest = (key: string) => {
+            return json.tests[key][
+                Math.floor(Math.random() * json.tests[key].length)
+            ];
+        };
 
-    beforeAll(() => {
-        config.IsAutomatedTesting = false; // Disable automated testing to allow sending requests to the APIs
-    });
+        beforeAll(() => {
+            config.IsAutomatedTesting = false; // Disable automated testing to allow sending requests to the APIs
+        });
 
-    describe('Bancho tests', () => {
-        const hasClientToken =
-            config.BANCHO_CLIENT_ID && config.BANCHO_CLIENT_SECRET
-                ? true
-                : false;
+        describe('Bancho tests', () => {
+            const hasClientToken =
+                config.BANCHO_CLIENT_ID && config.BANCHO_CLIENT_SECRET
+                    ? true
+                    : false;
 
-        it.skipIf(!config.IsProduction)(
-            'Bancho: should return converted beatmap',
-            async () => {
+            it('Bancho: should return converted beatmap', async () => {
                 if (!hasClientToken) {
                     return it.skip('Bancho: should return converted beatmap', () => {});
                 }
@@ -53,12 +53,9 @@ describe('Mirror tests', () => {
                 expect(beatmap.result).toContainAllKeys(
                     Object.keys(randomTest.data),
                 );
-            },
-        );
+            });
 
-        it.skipIf(!config.IsProduction)(
-            'Bancho: should return converted beatmapset',
-            async () => {
+            it('Bancho: should return converted beatmapset', async () => {
                 if (!hasClientToken) {
                     return it.skip('Bancho: should return converted beatmapset', () => {});
                 }
@@ -73,14 +70,11 @@ describe('Mirror tests', () => {
                 expect(beatmap.result).toContainAllKeys(
                     Object.keys(randomTest.data),
                 );
-            },
-        );
-    });
+            });
+        });
 
-    describe('Mino tests', () => {
-        it.skipIf(!config.IsProduction)(
-            'Mino: should return converted beatmap',
-            async () => {
+        describe('Mino tests', () => {
+            it('Mino: should return converted beatmap', async () => {
                 const randomTest = getRandomTest('getBeatmapById');
                 const { beatmapId } = randomTest;
 
@@ -91,12 +85,9 @@ describe('Mirror tests', () => {
                 expect(beatmap.result).toContainAllKeys(
                     Object.keys(randomTest.data),
                 );
-            },
-        );
+            });
 
-        it.skipIf(!config.IsProduction)(
-            'Mino: should return converted beatmapset',
-            async () => {
+            it('Mino: should return converted beatmapset', async () => {
                 const randomTest = getRandomTest('getBeatmapsetById');
                 const { beatmapSetId } = randomTest;
 
@@ -107,12 +98,9 @@ describe('Mirror tests', () => {
                 expect(beatmap.result).toContainAllKeys(
                     Object.keys(randomTest.data),
                 );
-            },
-        );
+            });
 
-        it.skipIf(!config.IsProduction)(
-            'Mino: should download beatmap set without video',
-            async () => {
+            it('Mino: should download beatmap set without video', async () => {
                 const randomTest = getRandomTest('downloadBeatmapSet');
                 const { beatmapSetId } = randomTest;
 
@@ -129,13 +117,9 @@ describe('Mirror tests', () => {
                     expected.byteLength - 1000,
                     expected.byteLength + 1000,
                 );
-            },
-            30000,
-        );
+            }, 30000);
 
-        it.skipIf(!config.IsProduction)(
-            'Mino: should download beatmap set with video',
-            async () => {
+            it('Mino: should download beatmap set with video', async () => {
                 const randomTest = getRandomTest('downloadBeatmapSet');
                 const { beatmapSetId } = randomTest;
 
@@ -152,15 +136,11 @@ describe('Mirror tests', () => {
                     expected.byteLength - 1000,
                     expected.byteLength + 1000,
                 );
-            },
-            30000,
-        );
-    });
+            }, 30000);
+        });
 
-    describe('Osulabs tests', () => {
-        it.skipIf(!config.IsProduction)(
-            'Osulabs: should return converted beatmap',
-            async () => {
+        describe('Osulabs tests', () => {
+            it('Osulabs: should return converted beatmap', async () => {
                 const randomTest = getRandomTest('getBeatmapById');
                 const { beatmapId } = randomTest;
 
@@ -171,12 +151,9 @@ describe('Mirror tests', () => {
                 expect(beatmap.result).toContainAllKeys(
                     Object.keys(randomTest.data),
                 );
-            },
-        );
+            });
 
-        it.skipIf(!config.IsProduction)(
-            'Osulabs: should return converted beatmapset',
-            async () => {
+            it('Osulabs: should return converted beatmapset', async () => {
                 const randomTest = getRandomTest('getBeatmapsetById');
                 const { beatmapSetId } = randomTest;
 
@@ -187,12 +164,9 @@ describe('Mirror tests', () => {
                 expect(beatmap.result).toContainAllKeys(
                     Object.keys(randomTest.data),
                 );
-            },
-        );
+            });
 
-        it.skipIf(!config.IsProduction)(
-            'Osulabs: should download beatmap set without video',
-            async () => {
+            it('Osulabs: should download beatmap set without video', async () => {
                 const randomTest = getRandomTest('downloadBeatmapSet');
                 const { beatmapSetId } = randomTest;
 
@@ -209,13 +183,9 @@ describe('Mirror tests', () => {
                     expected.byteLength - 1000,
                     expected.byteLength + 1000,
                 );
-            },
-            30000,
-        );
+            }, 30000);
 
-        it.skipIf(!config.IsProduction)(
-            'Osulabs: should download beatmap set with video',
-            async () => {
+            it('Osulabs: should download beatmap set with video', async () => {
                 const randomTest = getRandomTest('downloadBeatmapSet');
                 const { beatmapSetId } = randomTest;
 
@@ -232,15 +202,11 @@ describe('Mirror tests', () => {
                     expected.byteLength - 1000,
                     expected.byteLength + 1000,
                 );
-            },
-            30000,
-        );
-    });
+            }, 30000);
+        });
 
-    describe('Direct tests', () => {
-        it.skipIf(!config.IsProduction)(
-            'Direct: should download beatmap set without video',
-            async () => {
+        describe('Direct tests', () => {
+            it('Direct: should download beatmap set without video', async () => {
                 const randomTest = getRandomTest('downloadBeatmapSet');
                 const { beatmapSetId } = randomTest;
 
@@ -257,13 +223,9 @@ describe('Mirror tests', () => {
                     expected.byteLength - 1000,
                     expected.byteLength + 1000,
                 );
-            },
-            30000,
-        );
+            }, 30000);
 
-        it.skipIf(!config.IsProduction)(
-            'Direct: should download beatmap set with video',
-            async () => {
+            it('Direct: should download beatmap set with video', async () => {
                 const randomTest = getRandomTest('downloadBeatmapSet');
                 const { beatmapSetId } = randomTest;
 
@@ -280,15 +242,11 @@ describe('Mirror tests', () => {
                     expected.byteLength - 1000,
                     expected.byteLength + 1000,
                 );
-            },
-            30000,
-        );
-    });
+            }, 30000);
+        });
 
-    describe('Gatari tests', () => {
-        it.skipIf(!config.IsProduction)(
-            'Gatari: should download beatmap set without video',
-            async () => {
+        describe('Gatari tests', () => {
+            it('Gatari: should download beatmap set without video', async () => {
                 const randomTest = getRandomTest('downloadBeatmapSet');
                 const { beatmapSetId } = randomTest;
 
@@ -305,15 +263,11 @@ describe('Mirror tests', () => {
                     expected.byteLength - 1000,
                     expected.byteLength + 1000,
                 );
-            },
-            30000,
-        );
-    });
+            }, 30000);
+        });
 
-    describe('Nerinyan tests', () => {
-        it.skipIf(!config.IsProduction)(
-            'Nerinyan: should download beatmap set without video',
-            async () => {
+        describe('Nerinyan tests', () => {
+            it('Nerinyan: should download beatmap set without video', async () => {
                 const randomTest = getRandomTest('downloadBeatmapSet');
                 const { beatmapSetId } = randomTest;
 
@@ -330,13 +284,9 @@ describe('Mirror tests', () => {
                     expected.byteLength - 1000,
                     expected.byteLength + 1000,
                 );
-            },
-            30000,
-        );
+            }, 30000);
 
-        it.skipIf(!config.IsProduction)(
-            'Nerinyan: should download beatmap set with video',
-            async () => {
+            it('Nerinyan: should download beatmap set with video', async () => {
                 const randomTest = getRandomTest('downloadBeatmapSet');
                 const { beatmapSetId } = randomTest;
 
@@ -353,8 +303,7 @@ describe('Mirror tests', () => {
                     expected.byteLength - 1000,
                     expected.byteLength + 1000,
                 );
-            },
-            30000,
-        );
-    });
-});
+            }, 30000);
+        });
+    },
+);
